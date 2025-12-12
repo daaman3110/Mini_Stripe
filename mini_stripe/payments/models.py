83f127013be0cba5a3a9e4313894439be8d3cebb
daaman3110 <daaman3110@gmail.com>
2025-12-12 20:13:25 +0530

    Added DB Models

--- mini_stripe/payments/models.py
+++ mini_stripe/payments/models.py
@@ -1,3 +1,41 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.email}"
+
+
+class PaymentIntent(models.Model):
+    class StatusChoices(models.TextChoices):
+        CREATED = "created", "Created"
+        PROCESSING = "processing", "Processing"
+        SUCCEEDED = "succeeded", "Succeeded"
+        FAILED = "failed", "Failed"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3)
+    status = models.CharField(
+        max_length=20, choices=StatusChoices.choices, default=StatusChoices.CREATED
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Payment {self.id} - {self.status}"
+
+class WebhookEventLog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    event_name = models.CharField(max_length=100)
+    payment_intent = models.ForeignKey(PaymentIntent, on_delete=models.CASCADE)
+    payload = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
