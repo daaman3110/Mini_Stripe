38c19b4e2c1c0e13f6ed43cf1cceeac12d7551d2
daaman3110 <daaman3110@gmail.com>
2025-12-13 19:22:22 +0530

    Added ViewSet for Customer Class

--- mini_stripe/payments/views.py
+++ mini_stripe/payments/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Customer
+from .serializers import CustomerSerializer
+
 
 # Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [AllowAny]
