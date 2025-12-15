a3157cdb5177d67a2532677d466bf2061e9a4e64
daaman3110 <daaman3110@gmail.com>
2025-12-15 13:36:01 +0530

    Added PaymentIntent Serializer and its corresponding Views

--- mini_stripe/payments/serializers.py
+++ mini_stripe/payments/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, PaymentIntent
 
 
+# Customer Serializer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+
+# PaymentIntent Serializer
+class PaymentIntentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentIntent
+        fields = "__all__"
+        read_only_fields = ["id", "status", "created_at", "updated_at"]
--- mini_stripe/payments/views.py
+++ mini_stripe/payments/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-from .models import Customer
-from .serializers import CustomerSerializer
+from .models import Customer, PaymentIntent
+from .serializers import CustomerSerializer, PaymentIntentSerializer
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 
 # Create your views here.
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [AllowAny]
+
+
+# TO create POST /payment_intents/ endpoint
+class PaymentIntentCreateView(CreateAPIView):
+    queryset = PaymentIntent.objects.all()
+    serializer_class = PaymentIntentSerializer
+    permission_classes = [AllowAny]
+
+
+# To create GET  /payment_intents/<id>/
+class PaymentIntentDetailView(RetrieveAPIView):
+    queryset = PaymentIntent.objects.all()
+    serializer_class = PaymentIntentSerializer
     permission_classes = [AllowAny]
